c507670aa7a630d6872a49ddb1741d882fe4091e
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-02-11 17:24:16 -0400

    fix: adjust get alll polls

--- src/http/routes/get-all-polls.ts
+++ src/http/routes/get-all-polls.ts
@@ -1,10 +1,67 @@
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
+type Poll = {
+  id: "";
+  title: "";
+  createdAt: Date;
+  updatedAt: Date;
+  options: [
+    {
+      id: "";
+      title: "";
+      score: number;
+    }
+  ];
+};
 export async function getAllPolls(app: FastifyInstance) {
   app.get("/polls", async (request, reply) => {
-    const polls = await prisma.poll.findMany();
+    const polls = await prisma.poll.findMany({
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true,
+            score: true,
+          },
+        },
+      },
+    });
 
-    return reply.send({ polls });
+    const returnPolls: Poll[] = polls as Poll[];
+
+    polls.map(async (poll, id) => {
+      const result = await redis.zrange(poll.id, 0, -1, "WITHSCORES");
+
+      const votes = result.reduce((obj, line, index) => {
+        if (index % 2 === 0) {
+          const score = result[index + 1];
+          Object.assign(obj, { [line]: Number(score) });
+        }
+        return obj;
+      }, {} as Record<string, number>);
+
+      poll.options.map((option, i) => {
+        returnPolls.map((returnPoll) => {
+          returnPoll.options.map((optionReturn) => {
+            console.log('teste');
+            
+            optionReturn.score =
+              option.id in votes ? votes[optionReturn.id] : 0;
+          });
+        });
+        //polls[id].options[i] = {
+        //  id: option.id,
+        //  title: option.title,
+        //  score: option.id in votes ? votes[option.id] : 0,
+        //};
+      });
+    });
+    returnPolls.map(r=>{
+      console.log(r);
+      
+    })
+    return reply.send(returnPolls);
   });
 }
