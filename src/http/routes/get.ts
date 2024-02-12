82e5b5e607585f8fd59f3e55716b3fff8f1352fe
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-02-12 17:31:13 -0400

    update get

--- src/http/routes/get.ts
+++ src/http/routes/get.ts
@@ -5,8 +5,20 @@ import { redis } from "../../lib/redis";
 
 export async function get(app: FastifyInstance) {
   app.get("/", async (request, reply) => {
+    const polls = await prisma.poll.findMany({
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true,
+            score: true,
+          },
+        },
+      },
+    });
     return reply.send({
       message: "Server is running",
+      polls,
     });
   });
 }
