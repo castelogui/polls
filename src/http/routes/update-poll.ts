a7899ad8c6748821eacd9c7618aafdeb6d187181
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-02-12 00:13:23 -0400

    fix: adjust update polls

--- src/http/routes/update-poll.ts
+++ src/http/routes/update-poll.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
-
 export async function updatePoll(app: FastifyInstance) {
   app.patch("/polls/:pollId", async (request, reply) => {
     const updatePollParams = z.object({
@@ -20,32 +19,56 @@ export async function updatePoll(app: FastifyInstance) {
     const { pollId } = updatePollParams.parse(request.params);
     const { title, options } = updatePollBody.parse(request.body);
 
-    options.map(async (option) => {
+    // Deletar os votos associados às opções de enquete
+    const deleteVotesPromises = options.map(async (option) => {
       if (option.title == "") {
-        await prisma.pollOption.delete({ where: { id: option.id } });
+        try {
+          await prisma.vote.deleteMany({
+            where: { pollOptionId: option.id },
+          });
+        } catch (error) {
+          console.log(error);
+        }
       }
     });
 
-    options.map(async (option) => {
-      try {
-        const update = await prisma.pollOption.update({
-          where: { id: option.id },
-          data: {
-            title: option.title,
-          },
-        });
-        return update;
-      } catch (error) {
-        console.log(error);
+    // Esperar todas as operações de deletar votos serem concluídas
+    await Promise.all(deleteVotesPromises);
+
+    // Deletar as opções de enquete
+    const deleteOptionsPromises = options.map(async (option) => {
+      if (option.title == "") {
+        try {
+          await prisma.pollOption.delete({ where: { id: option.id } });
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        try {
+          await prisma.pollOption.update({
+            where: { id: option.id },
+            data: { title: option.title },
+          });
+        } catch (error) {
+          console.log(error);
+        }
       }
     });
 
-    await prisma.poll.update({
-      where: { id: pollId },
-      data: {
-        title: title,
-      },
-    });
+    // Esperar todas as operações de deletar opções serem concluídas
+    await Promise.all(deleteOptionsPromises);
+
+    // Atualizar o título da enquete
+    try {
+      await prisma.poll.update({
+        where: { id: pollId },
+        data: {
+          title: title,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
 
     return reply.status(201).send();
   });
