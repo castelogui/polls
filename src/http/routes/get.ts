9396b2678c2967303f0bfaebf27fa1756d2f3084
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-02-13 17:52:21 -0400

    remove get all polls

--- src/http/routes/get.ts
+++ src/http/routes/get.ts
@@ -1,22 +1,21 @@
-import { prisma } from "../../lib/prisma";
+//import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
 
 export async function get(app: FastifyInstance) {
   app.get("/", async (request, reply) => {
-    const polls = await prisma.poll.findMany({
-      include: {
-        options: {
-          select: {
-            id: true,
-            title: true,
-            score: true,
-          },
-        },
-      },
-    });
+    //const polls = await prisma.poll.findMany({
+    //  include: {
+    //    options: {
+    //      select: {
+    //        id: true,
+    //        title: true,
+    //        score: true,
+    //      },
+    //    },
+    //  },
+    //});
     return reply.send({
       message: "Server is running",
-      polls,
     });
   });
 }
